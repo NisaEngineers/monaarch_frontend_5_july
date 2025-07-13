fe02605fa593dfc48387b8c43195dc2ef88854e3
NisaEngineers <71565114+NisaEngineers@users.noreply.github.com>
2025-07-13 15:04:48 +0600

    Update logo.tsx

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -2,9 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/images/logo.svg";
 
-export default function Logo() {
+type LogoProps = {
+  className?: string;
+};
+
+export default function Logo({ className }: LogoProps) {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
+    <Link href="/" className={`inline-flex shrink-0 ${className || ""}`} aria-label="Cruip">
       <Image src={logo} alt="Cruip Logo" width={60} height={60} />
     </Link>
   );
